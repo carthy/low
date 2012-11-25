a10ac40522bd540ad8bb71de6d38df98a40b68bc
Bronsa <brobronsa@gmail.com>
2012-11-25 23:48:26 +0100

    jna: type-check functions

--- src/low/jna.clj
+++ src/low/jna.clj
@@ -1,5 +1,34 @@
 (ns low.jna
-  (:import (com.sun.jna Function NativeLibrary)))
+  (:import (com.sun.jna Function NativeLibrary Pointer)))
+
+(defn get-keys [coll]
+  (if (map? coll)
+    (keys coll)
+    (range (count coll))))
+
+(defn get-values [coll]
+  (if (map? coll)
+    (vals coll)
+    coll))
+
+(defmacro def-enum [name coll]
+  `(let [coll# ~coll
+         r-coll# (zipmap (get-values coll#) (get-keys coll#))]
+     (def ~name
+       {:name '~name
+        :type ~Integer
+        :fn (fn [key#]
+              (if (number? key#)
+                (coll# key#)
+                (r-coll# key#)))})))
+
+(defmacro def-type-alias [name type]
+  `(def ~name {:name '~name :type ~type}))
+
+(defmacro def-pointers [& aliases]
+  (cons 'do
+        (for [alias aliases]
+          `(def-type-alias ~alias Pointer))))
 
 (defn load-lib [lib]
   (NativeLibrary/getInstance lib))
@@ -9,8 +38,37 @@
     (Function/getFunction lib name)
     (.getFunction ^NativeLibrary lib name)))
 
-(defn import-function [lib name ret-type args-len]
-  (let [f (get-function lib name)]
-    (fn [& args]
-      (assert (== args-len (count args)))
-      (.invoke f ret-type (to-array args)))))
+(defn get-type [t]
+  (if (map? t)
+     (get-type (:type t))
+     (if (class? t)
+       t
+       (class t))))
+
+(defn get-type* [t]
+  (let [t (get-type t)]
+    (if (or (= Integer t)
+            (= Long t))
+      Number
+      t)))
+
+(defn matching-types [args r]
+  (every? true? (map = (map get-type* args)
+                     (map get-type* r))))
+
+(defn adjust [args r]
+  (let [ret (map #(if (map? %) (:ret %) %) r)]
+    (map #((or (when (map? %) (:fn %))
+               identity)
+           %2) args ret)))
+
+(defn import-function [lib name args ret-type]
+  (let [f (get-function lib name)
+        ret-class (get-type ret-type)
+        fun (when (map? ret-type)
+              (:fn ret-type))]
+    (fn [& r]
+      (assert (matching-types args r))
+      {:ret ((or fun identity)
+             (.invoke f ret-class (to-array (adjust args r))))
+       :type ret-type})))
--- src/low/llvm.clj
+++ src/low/llvm.clj
@@ -1,5 +1,5 @@
 (ns low.llvm
-  (:require [low.jna :refer [import-function load-lib]])
+  (:require [low.jna :refer :all] :reload-all)
   (:import (com.sun.jna Pointer)))
 
 (load "llvm_api")
@@ -7,19 +7,14 @@
 (def ^:private llvm-function-map (atom {}))
 (def ^:private llvm-lib (promise))
 
-(defn import-llvm-function [f-name ret-type args-len]
-  (let [[ret-type wrapping-fun]
-        (if (class? ret-type)
-          [ret-type identity]
-          [Integer (fn [f] (fn [& args] (ret-type (apply f args))))])]
-    (swap! llvm-function-map assoc f-name
-           (wrapping-fun
-            (import-function @llvm-lib (str "LLVM" (name f-name)) ret-type args-len)))))
+(defn import-llvm-function [f-name args ret-type]
+  (swap! llvm-function-map assoc f-name
+         (import-function @llvm-lib (str "LLVM" (name f-name)) args ret-type)))
 
 (defn setup-llvm [ver]
   (deliver llvm-lib (load-lib (str "LLVM-" ver)))
-  (doseq [[f-name ret-type args-len] llvm-api]
-    (import-llvm-function f-name ret-type args-len)))
+  (doseq [[f-name args ret-type] llvm-api]
+    (import-llvm-function f-name args ret-type)))
 
 (defn LLVM [f & args]
   (if-let [f (@llvm-function-map f)]
