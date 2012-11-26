c146089bc54edbad23e1946054d872082e069619
Bronsa <brobronsa@gmail.com>
2012-11-26 17:19:06 +0100

    llvm: make the apis version-aware

--- src/low/llvm.clj
+++ src/low/llvm.clj
@@ -1,20 +1,32 @@
 (ns low.llvm
-  (:require [low.jna :refer :all] :reload-all)
+  (:require [low.jna :refer :all :rename {def-enum def-enum*}])
   (:import (com.sun.jna Pointer)))
-
-(load "llvm_api")
 
 (def ^:private llvm-function-map (atom {}))
 (def ^:private llvm-lib (promise))
+(def ^:private llvm-version (promise))
 
 (defn import-llvm-function [f-name args ret-type]
   (swap! llvm-function-map assoc f-name
          (import-function @llvm-lib (str "LLVM" (name f-name)) args ret-type)))
 
+(defmacro def-enum [n & args]
+  (let [enum-versioned (partition 2 args)
+        enum-map (reduce #(reduce (fn [ret k] (assoc ret k (second %2)))
+                                  % (first %2))
+                         {} enum-versioned)]
+    `(def-enum* ~n
+       ~(or (enum-map @llvm-version) {}))))
+
+(declare llvm-api)
+
 (defn setup-llvm [ver]
+  (deliver llvm-version ver)
   (deliver llvm-lib (load-lib (str "LLVM-" ver)))
-  (doseq [[f-name args ret-type] llvm-api]
-    (import-llvm-function f-name args ret-type)))
+  (load "llvm_api")
+  (doseq [[f-name args ret-type versions] llvm-api]
+    (when (versions ver)
+        (import-llvm-function f-name args ret-type))))
 
 (defn LLVM [f & args]
   (if-let [f (@llvm-function-map f)]
