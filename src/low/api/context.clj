f53a9ba9a526e728da8ec2c77f55579bcc561d47
Bronsa <brobronsa@gmail.com>
2012-11-26 20:27:38 +0100

    low/api: add module and context

---
+++ src/low/api/context.clj
@@ -0,0 +1,25 @@
+(ns low.api.context
+  (:require [low.llvm :refer [LLVM]]))
+
+(defn create []
+  (LLVM :ContextCreate))
+
+(defn global-context
+  ([]
+     (LLVM :GetGlobalContext))
+  ([module]
+     (LLVM :GetModuleContext module)))
+
+(defn destroy! [context]
+  (LLVM :ContextDispose context))
+
+(defmacro with-contexts [[& ctxs] & body]
+  `(let [~@(mapcat #(list % `(create)) ctxs)]
+     (try ~@body
+          (finally ~@(map #(list `destroy! %) ctxs)))))
+
+(defn metadata-kind-id
+  ([string]
+     (LLVM :GetMDKindID string (count string)))
+  ([context string]
+     (LLVM :GetMDKindIDInContext context string (count string))))
---
+++ src/low/api/module.clj
@@ -0,0 +1,70 @@
+(ns low.api.module
+  (:require [low.llvm :refer LLVM]))
+
+(defn create
+  ([^String name]
+     (LLVM :ModuleCreateWithName name))
+  ([^String name context]
+     (LLVM :ModuleCreateWithNameInContext name context)))
+
+(defn destroy! [module]
+  (LLVM :DisposeModule module))
+
+(defmacro with-modules [[& ctxs] & body]
+  `(let [~@(mapcat #(list % `(create)) ctxs)]
+     (try ~@body
+          (finally ~@(map #(list `destroy! %) ctxs)))))
+
+(defn data-layout [module]
+  (LLVM :GetDataLayout module))
+
+(defn data-layout! [module triple]
+  (LLVM :SetDataLayout module triple))
+
+(defn target [module]
+  (LLVM :GetTarget module))
+
+(defn target! [module triple]
+  (LLVM :SetTarget module triple))
+
+(defn dump! [module]
+  (LLVM :DumpModule module))
+
+(defn inline-asm! [module asm]
+  (LLVM :SetModuleInlineAsm module asm))
+
+(defn context [module]
+  (LLVM :GetModuleContext module))
+
+(defn type [module name]
+  (LLVM :GetTypeByName module name))
+
+(defn add-function [module name function]
+  (LLVM :AddFunction module name function))
+
+(defn function [module name]
+  (LLVM :GetNamedFunction module name))
+
+(defn first-function [module]
+  (LLVM :GetFirstFunction module))
+
+(defn last-function [module]
+  (LLVM :GetLastFunction module))
+
+(defn add-global
+  ([module type name]
+     (LLVM :AddGlobal module type name))
+  ([module type name address-space]
+     (LLVM :AddGlobalInAddressSpace module type name address-space)))
+
+(defn global [module name]
+  (LLVM :GetNamedGlobal module name))
+
+(defn first-global [module]
+  (LLVM :GetFirstGlobal module))
+
+(defn last-global [module]
+  (LLVM :GetLastGlobal module))
+
+(defn add-alias [module type value name]
+  (LLVM :AddAlias module type value name))
