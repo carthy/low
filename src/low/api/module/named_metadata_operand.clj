db777e9ef366dc702fe4cf037dec89488092757f
Bronsa <brobronsa@gmail.com>
2012-12-16 19:50:18 +0100

    api/module: moved named-metadata to named-metadata-operand

--- src/low/api/module/named_metadata_operand.clj
+++ src/low/api/module/named_metadata_operand.clj
@@ -1,18 +1,19 @@
-(ns low.api.module.named-metadata
+(ns low.api.module.named-metadata-operand
+  (:refer-clojure :exclude [count])
   (:require [low.llvm :refer [LLVM llvm-version]]
             [low.native :refer [pointer to-ptr-vec]]))
 
-(defn operand! [module name val]
+(defn add [module name val]
   {:pre [(>= @llvm-version 3.2)]}
   (LLVM :AddNamedMetadataOperand module name val))
 
-(defn operands-count [module name]
+(defn count [module name]
   {:pre [(>= @llvm-version 3.2)]}
   (LLVM :GetNamedMetadataNumOperands module name))
 
 (defn operands [module name]
   {:pre [(>= @llvm-version 3.2)]}
-  (let [count @(operands-count module name)
+  (let [count @(count module name)
         ret (pointer :value count)]
     (LLVM :GetNamedMetadataOperands module name ret)
     (to-ptr-vec ret count)))
