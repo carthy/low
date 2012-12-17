bde98436306d3a28d48974754d3beb69298cca18
Bronsa <brobronsa@gmail.com>
2012-12-17 22:06:58 +0100

    api/value/constant: added scalar constants

---
+++ src/low/api/value/constant.clj
@@ -0,0 +1,43 @@
+(ns low.api.value.constant
+  (:require [low.llvm :refer [LLVM]]
+            [low.native :refer [array-of]]))
+
+(defmacro ^:private LLVMC [op & args]
+  `(LLVM ~(->> op name (str "Const") keyword) ~@args))
+
+(defn null [type]
+  (LLVMC :Null type))
+
+(defn null-pointer [type]
+  (LLVMC :PointerNull type))
+
+(defn all-ones [type]
+  (LLVMC :AllOnes type))
+
+(defn undef [type]
+  (LLVM :GetUndef type))
+
+(defn integer
+  ([type num c]
+     (if (number? num)
+       (LLVMC :Int type num c)
+       (if (string? num)
+         (LLVMC :IntOfString type num c)
+         (LLVMC :IntOfArbitraryPrecision type num (array-of :__int64 c)))))
+  ([type num len radix]
+     (LLVMC :IntOfStringAndSize type num len radix)))
+
+
+(defn floating
+  ([type num]
+     (if (number? num)
+       (LLVMC :Real type num)
+       (LLVMC :RealOfString type num)))
+  ([type num size]
+     (LLVMC :RealOfStringAndSize type num size)))
+
+(defn int-z-ext-value [value]
+  (LLVMC :IntGetZExtValue value))
+
+(defn int-s-ext-value [value]
+  (LLVMC :IntGetSExtValue value))
