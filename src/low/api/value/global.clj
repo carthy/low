865f6654489aa1aac43db99994037ca2be46803d
Bronsa <brobronsa@gmail.com>
2012-12-19 00:32:24 +0100

    api/value: add global

---
+++ src/low/api/value/global.clj
@@ -0,0 +1,32 @@
+(ns low.api.value.global
+  (:require [low.llvm :refer [LLVM]]))
+
+(defn parent [global]
+  (LLVM :GetGlobalParent global))
+
+(defn declaration? [global]
+  (LLVM :IsDeclaration global))
+
+(defn linkage [global]
+  (LLVM :GetLinkage global))
+
+(defn linkage! [global linkage]
+  (LLVM :SetLinkage global linkage))
+
+(defn section [global]
+  (LLVM :GetSection global))
+
+(defn section! [global section]
+  (LLVM :SetSection global section))
+
+(defn visibility [global]
+  (LLVM :GetVisibility global))
+
+(defn visibility! [global visibility]
+  (LLVM :SetVisibility global visibility))
+
+(defn alignment [global]
+  (LLVM :GetAlignment global))
+
+(defn alignment! [global bytes]
+  (LLVM :SetAlignment global bytes))
