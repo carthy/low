11af16d5b4aa15418abedd5f6f0a835280efc0d5
Bronsa <brobronsa@gmail.com>
2012-12-18 16:17:10 +0100

    api/value/constants: add expr and block-address

--- src/low/api/value/constant.clj
+++ src/low/api/value/constant.clj
@@ -70,3 +70,51 @@
 
 (defn vector [vals size]
   (LLVMC :Vector vals size))
+
+
+(def exprs #{:neg :nsw-neg :nuw-neg :f-neg
+             :add :nsw-add :nuw-add :f-add
+             :sub :nsw-sub :nuw-sub :f-sub
+             :mul :nsw-mul :nuw-mul :f-mul
+             :u-div :s-div :exact-s-div :f-div
+             :u-rem :s-rem :f-rem
+             :not :and :or :xor :cmp :f-cmp :i-cmp
+             :shl :l-shr :a-shr :get-element-ptr
+             :in-bounds-get-element-ptr
+             :extract-element :insert-element
+             :insert-value :select
+             :shuffle-vector :fp-cast :bit-cast :int-cast
+             :ptr-cast :s-ext-or-bit-cast :trunc-or-bit-cast
+             :z-ext-or-bit-cast :inline-asm
+             :fp-ext :fp-to-si :fp-to-ui :fp-trunc :int-to-ptr
+             :ptr-to-int :s-ext :si-to-fp :trunc :ui-to-fp
+             :z-ext :extract-value :load :var-arg})
+
+(def ^:private special-type-name
+  {:nsw-neg "NSWNeg"
+   :nuw-neg "NUWNeg"
+   :nsw-add "NSWAdd"
+   :nuw-add "NUWAdd"
+   :nsw-sub "NSWSub"
+   :nuw-sub "NUWSub"
+   :nsw-mul "NSWMul"
+   :nuw-mul "NUWMul"
+   :get-element-ptr "GEP"
+   :in-bounds-get-element-ptr "InBoundsGEP"
+   :ptr-cast "PointerCast"
+   :fp-ext "FPExt"
+   :fp-to-ui "FPToUI"
+   :fp-to-si "FPToSI"
+   :fp-trunc "FPTrunc"
+   :si-to-fp "SIToFP"
+   :ui-to-fp "UIToFP"})
+
+
+(defn expr [expr & args]
+  (apply LLVMC (keyword
+                (or (special-type-name expr)
+                    (s/join (map s/capitalize (s/split (name expr) #"-")))))
+         args))
+
+(defn block-address [f basic-block]
+  (LLVM :BlockAddress f basic-block))
