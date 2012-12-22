1aebc5f7bb4c761a224354a49ebbe4ca58aeb19d
Bronsa <brobronsa@gmail.com>
2012-12-22 02:40:53 +0100

    api/execution-engine: fix jit, fix error return

--- src/low/api/execution_engine.clj
+++ src/low/api/execution_engine.clj
@@ -1,23 +1,24 @@
 (ns low.api.execution-engine
   (:require [low.llvm :refer [LLVM]]
-            [low.native :refer [pointer &]])
+            [low.native :refer [pointer to-str &]])
   (:import (com.sun.jna Memory Pointer)))
 
-(defn create [module & [type]]
+(defn create [module & [type opt-level]]
   {:pre [(or (not type) (#{:jit :interpreter} type))]}
   (let [err (pointer :char*)
-        engine (pointer :execution-engine-ref)
+        engine (pointer :execution-engine)
         action (if type
                  (if (= type :jit)
                    :CreateJITCompiler
                    :CreateInterpreter)
                  :CreateExecutionEngine)
-        action (if (= (:type module) :module-ref)
+        opt-level (or opt-level 3)
+        action (if (= (:type module) :module)
                  (keyword (str (name action) "ForModule"))
                  action)
-        ret (LLVM action  engine module err)]
+        ret (apply LLVM action engine module (if (= type :jit) [opt-level err] [err]))]
     (if @ret
-      (.getString (& err) 0)
+      (to-str (& err))
       engine)))
 
 (defn destroy! [engine]
